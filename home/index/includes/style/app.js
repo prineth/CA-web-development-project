fa1b1f1e53688f3f0850a3030cc3a977329ee313
prineth <prinethferandox@gmail.com>
2020-11-10 22:04:38 +0530

    add grow bar

--- home/index/includes/style/app.js
+++ home/index/includes/style/app.js
@@ -1,12 +1,12 @@
 const translate = document.querySelectorAll(".translate");
-const big_title = document.querySelector(".big-title");
+//const big_title = document.querySelector(".big-title");
 const header = document.querySelector("header");
 const shadow = document.querySelector(".shadow");
 const content = document.querySelector(".content");
 const section = document.querySelector("section");
 const image_container = document.querySelector(".imgContainer");
 const opacity = document.querySelectorAll(".opacity");
-const border = document.querySelector(".border");
+//const border = document.querySelector(".border");
 
 let header_height = header.offsetHeight;
 let section_height = section.offsetHeight;
@@ -24,11 +24,13 @@ window.addEventListener('scroll', () => {
         element.style.opacity = scroll / (sectionY.top + section_height);
     })
 
-    big_title.style.opacity = - scroll / (header_height / 2) + 1;
+    //big_title.style.opacity = - scroll / (header_height / 2) + 1;
     shadow.style.height = `${scroll * 0.5 + 300}px`;
 
     content.style.transform = `translateY(${scroll / (section_height + sectionY.top) * 50 - 50}px)`;
     image_container.style.transform = `translateY(${scroll / (section_height + sectionY.top) * -50 + 50}px)`;
 
-    border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
+    //the underline 
+
+    //border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
 })
