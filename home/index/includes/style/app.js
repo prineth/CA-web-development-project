63be096d0f32b4d19bc55d4125eacd04f8bfcd33
JayathJS <JayathJSgit@users.noreply.github.com>
2020-11-18 20:55:35 +0530

    js

--- home/index/includes/style/app.js
+++ home/index/includes/style/app.js
@@ -35,8 +35,8 @@ window.addEventListener('scroll', () => {
     //border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
 })
 
-/*//////////////////////////////////////////////////////////////////////
-if (myModal.onclick== true) 
+
+if (myModal.onclick) 
 {
   var modal = document.getElementById("myModal");
 
@@ -65,7 +65,7 @@ window.onclick = function(event) {
 
   
 }
- else if (myModals.onclick== true) 
+ else if (myModals.onclick) 
  {
   var modal = document.getElementById("myModals");
 
@@ -92,7 +92,6 @@ window.onclick = function(event) {
   }
 }
 }
-*/
 
 /**Log in**************************************************/
 //Get the modal
@@ -150,29 +149,3 @@ window.onclick = function(event) {
   }
 }
 */
-
-// Get the button that opens the modal
-if(var btn == document.getElementById("myBtn")){
-      //Get the modal
-      var modal = document.getElementById("myModal");
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
